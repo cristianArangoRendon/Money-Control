4dbe37fbffb8b0638258d06a5b9a177851738bf9
cristian.arango <cristian.arango@dms.ms>
2024-04-23 12:08:35 -0500

    Agregar archivos de proyecto.

---
+++ MoneyControl.Core/DTOs/Response/ResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MoneyControl.Core.DTOs.Response
+{
+    public class ResponseDTO
+    {
+        public bool IsSuccess { get; set; }
+        public bool Message { get; set; }
+        public Object Data { get; set; }
+    }
+}
